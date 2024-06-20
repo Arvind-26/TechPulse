4f134e1004462f4d7b600cbafc9de3ceaec9727c
arvind-26 <ak5943436@gmail.com>
2024-06-20 22:23:16 +0530

    solve errors

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,11 @@
-import Navv from '../components/nav'
+import Nav from '../components/Nav'
 import { AppProvider } from '../context/AppContext';
 import Foot from '../components/footer'
 import React from 'react';
 
 function MyApp({ Component, pageProps }) {
   return <AppProvider>
-    <Navv />
+    <Nav />
     <Component {...pageProps} />
     <Foot />
 
