d1c3031083fde264ed2453c69802cc756b4d37a8
arvind-26 <ak5943436@gmail.com>
2024-06-21 21:32:50 +0530

    api error

--- src/pages/api/getproducts.js
+++ src/pages/api/getproducts.js
@@ -2,7 +2,7 @@ import product from "../../models/product"
 import connectdb from "../../connection/connection"
 
 const handler = async (req,res)=>{
-    let products = await product.find()
+    let products = await product.find().limit( 20 )
     res.status(200).json({products})
 }
 export default connectdb(handler);
