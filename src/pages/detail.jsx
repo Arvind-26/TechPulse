07b5e6cc21d8c00e9d06704f1b72f732d2e3a030
arvind-26 <ak5943436@gmail.com>
2024-06-21 11:14:45 +0530

    solve errors

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import { useRouter } from "next/router"
 import React from 'react'
-import Review from "../components/review"
+import Review from "../components/Review"
 import { useContext } from 'react';
 import AppContext from '../context/AppContext';
 import Link from 'next/link'
