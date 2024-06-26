1b3c6e8f80ed7448433a5f3156c9c465925d145d
arvind-26 <ak5943436@gmail.com>
2024-06-26 12:53:00 +0530

    loading

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import mongoose from "mongoose"
 import up from "../models/up"
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import AppContext from '../context/AppContext';
 import Head from 'next/head';
 
 
-function logout() {
-  localStorage.removeItem("sharedValues")
-  window.location.replace('/')
-}
 
 
 const Profile = ({ users }) => {
+  const [loading, setLoading] = useState(false)
+  const [delLoading, setDelLoading] = useState(false)
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
   const { sharedValues } = useContext(AppContext);
   var fname;
@@ -27,9 +25,15 @@ const Profile = ({ users }) => {
       add = element.address
     }
   });
-
-
+  
+  function logout() {
+    setLoading(true)
+    localStorage.removeItem("sharedValues")
+    window.location.replace('/')
+  }
+  
   async function delete_acc() {
+    setDelLoading(true)
     await fetch(`${API_URL}/api/addup`, {
       method: 'DELETE',
       headers: {
@@ -65,8 +69,22 @@ const Profile = ({ users }) => {
           <label className='md:text-xl text-[#d30a03]'>{email}</label>
         </span>
       </div>
-      <button className=" text-white md:font-bold bg-[#d30a03] rounded-2xl py-1 px-5 text-xl md:mt-4" onClick={logout}>Log Out</button>
-      <button className=" text-white md:font-bold bg-[#d30a03] rounded-2xl py-1 px-5 text-xl mb-2 md:mt-2" onClick={delete_acc}>Delete Account</button>
+      {!loading?<button className=" text-white md:font-bold bg-[#d30a03] rounded-2xl py-1 px-5 text-xl md:mt-4" onClick={logout}>Log Out</button>:<div
+            className="mt-2 ml-2 md:mt-0 md:ml-4 inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-danger motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status">
+            <span
+              class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...</span
+            >
+          </div>}
+      {!delLoading?<button className=" text-white md:font-bold bg-[#d30a03] rounded-2xl py-1 px-5 text-xl mb-2 md:mt-2" onClick={delete_acc}>Delete Account</button>:<div
+            className="mt-2 ml-2 md:mt-0 md:ml-4 inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-danger motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status">
+            <span
+              class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...</span
+            >
+          </div>}
     </main></>
   )
 }
