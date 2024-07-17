71e952bb4ccd9a803cb2ce81f2bf6fba8ee2c7bb
arvind-26 <ak5943436@gmail.com>
2024-07-17 19:45:03 +0530

    add searchbar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,22 +5,34 @@ import Link from 'next/link'
 import mongoose from "mongoose"
 import product from '../models/product'
 import offer from '../models/offer'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Techpulse({ products, offers }) {
-  useEffect(() => {
-    
-    let i = 0
-    document.getElementById("banner").src = offers[i].img
-    setInterval(() => {
+  const [index, setIndex] = useState(0)
+  const [search, setSearch] = useState("")
+
+  function infocus() {
+    document.getElementById('search').classList.remove('hidden')
+  }
+
+  function focusoff() {
+    setTimeout(() => {
       try {
-        document.getElementById("banner").src = offers[i].img
-        i = (i + 1) % 4
+        document.getElementById('search').classList.add('hidden')
       } catch (error) {
       }
-    }, 3000);
+    }, 500);
+  }
 
-  }, []);
+  useEffect(() => {
+    document.getElementById("banner").src = offers[index].img
+    const interval = setInterval(() => {
+      document.getElementById("banner").src = offers[index].img
+      setIndex((prevIndex) => (prevIndex + 1) % offers.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [index])
+
   return (
     <div>
       <Head>
@@ -29,6 +41,28 @@ export default function Techpulse({ products, offers }) {
         <link rel="icon" href={img.src} sizes="any" />
       </Head>
       <main>
+
+        <div>
+          <div className="flex flex-col p-2 pt-6 m-h-screen">
+            <div className="bg-white items-center justify-between relative m-auto w-full md:w-2/3 flex rounded-full shadow-2xl p-2" style={{ top: "5px" }}>
+              <div className="bg-gray-600 p-2 mx-2 rounded-full">
+                <svg className="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                </svg>
+              </div>
+              <input className="font-bold uppercase rounded-full w-full py-4 pl-4 text-gray-700 bg-gray-100 leading-tight focus:outline-none focus:shadow-outline lg:text-sm text-xs" onChange={(e) => setSearch(e.target.value.toLowerCase())} type="text" placeholder="Search" autoComplete='off' onFocus={infocus} onBlur={focusoff} />
+              <div id='search' className=' hidden left-0 rounded-lg max-h-48 top-20 justify-center shadow-2xl bg-white absolute w-full overflow-auto' style={{ "scrollbar-width": "none" }}>
+                {products.filter((item) => {
+                  return search.toLowerCase() === '' ? item : item.name.toLowerCase().includes(search)
+                }).map((item) => {
+                  return <Link key={item._id} href={{ pathname: '/detail', query: { id: `${item._id}` } }}>
+                    <li className=' h-9 list-none hover:bg-slate-200 shadow-2xl flex justify-center items-center'>{item.name}</li>
+                  </Link>
+                })}</div>
+            </div>
+          </div>
+        </div>
+
         <div className="p-3 mt-3 flex flex-col items-center text-2xl md:text-3xl font-bold">
           <label className="">Offers Today</label>
           <Image id="banner" alt="Product" className="bannerslide h-96 w-full mt-2 mb-10 " src={"data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA="} width={100} height={100} />
